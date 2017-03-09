0af841161fe49a5b74080fcd6b38dd62fade195e
Zhen Zhang <izgzhen@gmail.com>
2017-03-09 09:19:03 +0800

    Simplify language

--- theories/clang/lang.v
+++ theories/clang/lang.v
@@ -14,6 +14,12 @@ Inductive bop :=
 
 Definition decls := list (ident * type).
 
+Definition tid := nat.
+
+(* TODO: Parameterize it *)
+Inductive prim :=
+| Pcli | Psti | Pswitch (i: tid).
+
 Inductive expr :=
 | Evalue (v: val)
 | Evar (x: ident)
@@ -24,38 +30,23 @@ Inductive expr :=
 | Efst (e: expr)
 | Esnd (e: expr)
 | Ecall (fid: ident) (args: list expr)
-| Ealloc (t: type) (e: expr).
-(* | Ecast (e: expr) (t: type) *) (* NOTE: either cast or index are not essential, we'll do it later *)
+| Ealloc (t: type) (e: expr)
+| Eassign (lhs: expr) (rhs: expr)
+| Eif (cond: expr) (s1 s2: expr)
+| Ewhile (cond: expr) (curcond: expr) (s: expr)
+| Erete (e: expr)
+| Eseq (s1 s2: expr).
 
-Definition tid := nat.
-
-(* TODO: Parameterize it *)
-Inductive prim :=
-| Pcli | Psti | Pswitch (i: tid).
-
-Inductive stmts :=
-| Sskip
-| Sprim (p: prim)
-| Sassign (lhs: expr) (rhs: expr)
-| Sif (cond: expr) (s1 s2: stmts)
-| Swhile (cond: expr) (curcond: expr) (s: stmts)
-| Sret
-| Srete (e: expr)
-| Sseq (s1 s2: stmts).
-
-Record function (def_t: Type) :=
+Record function :=
   Function {
       f_retty: type;
       f_params: decls;
-      f_def: def_t
+      f_body: expr
     }.
-
-Definition program (t: Type) := ident → option (function t).
 
 (* Operational Semantics *)
 
 Inductive exprctx :=
-| EKid
 | EKbinopr (op: bop) (re: expr)
 | EKbinopl (op: bop) (lv: val)
 | EKderef_typed (t: type)
@@ -63,28 +54,20 @@ Inductive exprctx :=
 | EKfst
 | EKsnd
 | EKcall (fid: ident) (vargs: list val) (args: list expr)
-| EKalloc (t: type).
-(* | EKcast (t: type) *)
+| EKalloc (t: type)
+| EKassignr (rhs: expr)
+| EKassignl (lhs: addr)
+| EKif (s1 s2: expr)
+| EKwhile (cond: expr) (s: expr)
+| EKrete.
 
-Inductive stmtsctx :=
-| SKassignr (rhs: expr)
-| SKassignl (lhs: addr)
-| SKif (s1 s2: stmts)
-| SKwhile (cond: expr) (s: stmts)
-| SKrete.
-
-Definition context : Type := (exprctx * stmtsctx).
-
-Inductive cureval :=
-| curs (s: stmts) | cure (e: expr).
-
-Record env {t: Type} :=
+Record env :=
   Env {
       lenv: smap (type * addr);
-      fenv: smap (function t)
+      fenv: smap function
     }.
 
-Fixpoint type_infer {t: Type} (ev: env) (e: expr) : option type :=
+Fixpoint type_infer (ev: env) (e: expr) : option type :=
   match e with
     | Evalue v => Some (type_infer_v v)
     | Evar x => p ← sget x (lenv ev); Some (p.1)
@@ -106,13 +89,13 @@ Fixpoint type_infer {t: Type} (ev: env) (e: expr) : option type :=
          | Some (Tprod _ t2) => Some t2
          | _ => None
        end)
-    | Ecall f args => f_retty t <$> sget f (fenv ev)
+    | Ecall f args => f_retty <$> sget f (fenv ev)
     | Ealloc t _ => Some (Tptr t)
+    | _ => Some Tvoid
   end.
 
 Definition fill_expr (e : expr) (Ke : exprctx) : expr :=
   match Ke with
-    | EKid => e
     | EKbinopr op re => Ebinop op e re
     | EKbinopl op lv => Ebinop op (Evalue lv) e
     | EKderef_typed t => Ederef_typed t e
@@ -121,24 +104,17 @@ Definition fill_expr (e : expr) (Ke : exprctx) : expr :=
     | EKsnd => Esnd e
     | EKcall f vargs args => Ecall f (map Evalue vargs ++ e :: args)
     | EKalloc t => Ealloc t e
+    | EKassignr rhs => Eassign e rhs
+    | EKassignl lhs => Eassign (Evalue (Vptr lhs)) e
+    | EKif s1 s2 => Eif e s1 s2
+    | EKwhile c s => Ewhile c e s
+    | EKrete => Erete e
   end.
-
-Definition fill_stmts (e : expr) (Ks : stmtsctx) : stmts :=
-  match Ks with
-    | SKassignr rhs => Sassign e rhs
-    | SKassignl lhs => Sassign (Evalue (Vptr lhs)) e
-    | SKif s1 s2 => Sif e s1 s2
-    | SKwhile c s => Swhile c e s
-    | SKrete => Srete e
-  end.
-
-Definition fill_ctx (e: expr) (K: context) : stmts :=
-  match K with (ke, ks) => fill_stmts (fill_expr e ke) ks end.
 
 Definition fill_ectxs := foldl fill_expr.
 
 Definition heap := gmap addr byteval.
-Definition text := gmap ident (function stmts).
+Definition text := gmap ident function.
 
 Record state :=
   State {
@@ -186,6 +162,147 @@ Definition alloc_heap (l: addr) (v: val) (σ: state) : state :=
   State (storebytes l (encode_val v) (s_heap σ)) (s_text σ).
 
 (* expr *can* have side-effect *)
+
+Definition to_val (c: expr) :=
+  match c with
+    | Evalue v => Some v
+    | _ => None
+  end.
+
+Lemma of_to_val e v : to_val e = Some v → e = Evalue v.
+Admitted.
+
+Fixpoint to_ret_val (e: expr) :=
+  match e with
+    | Erete (Evalue v) => Some v
+    | _ => None
+  end.
+
+Lemma fill_not_val Kes e: to_val (fill_ectxs e Kes) = None.
+Admitted.
+
+Lemma fill_not_ret_val Kes e: to_ret_val (fill_ectxs e Kes) = None.
+Admitted.
+
+Lemma val_implies_not_ret_val c v:
+  to_val c = Some v → to_ret_val c = None.
+Proof. intros ?. destruct c =>//. Qed.
+
+Lemma ret_val_implies_not_val c v:
+  to_ret_val c = Some v → to_val c = None.
+Proof. intros ?. destruct c=>//. Qed.
+
+Fixpoint params_match (params: decls) (vs: list val) :=
+  match params, vs with
+    | (_, t)::params, v::vs => typeof v t ∧ params_match params vs
+    | [], [] => True
+    | _, _ => False
+  end.
+
+Fixpoint lift_list_option {A} (l: list (option A)): option (list A) :=
+  match l with
+    | Some x :: l' => (x ::) <$> lift_list_option l'
+    | None :: _ => None
+    | _ => Some []
+  end.
+
+Fixpoint resolve_rhs (ev: env) (e: expr) : option expr :=
+  match e with
+    | Evalue v => Some e
+    | Ederef_typed t e => Some e
+    | Ealloc t e => Ealloc t <$> resolve_rhs ev e
+    | Evar x => (* closed-ness? *)
+      (match sget x (lenv ev) with
+         | Some (t, l) => Some $ Ederef_typed t (Evalue (Vptr l))
+         | None => None
+       end)
+    | Ebinop op e1 e2 =>
+      match resolve_rhs ev e1, resolve_rhs ev e2 with
+        | Some e1', Some e2' => Some (Ebinop op e1' e2')
+        | _, _ => None
+      end
+    | Ederef e =>
+      match type_infer ev e, resolve_rhs ev e with
+        | Some (Tptr t), Some e' => Some (Ederef_typed t e') 
+        | _, _ => None
+      end
+    | Eaddrof e => Eaddrof <$> resolve_rhs ev e
+    | Efst e => Efst <$> resolve_rhs ev e
+    | Esnd e => Esnd <$> resolve_rhs ev e
+    | Ecall f args => Ecall f <$> lift_list_option (map (resolve_rhs ev) args)
+    | Eassign e1 e2 => Eassign e1 <$> resolve_rhs ev e2
+    | Eif e s1 s2 =>
+      match resolve_rhs ev e, resolve_rhs ev s1, resolve_rhs ev s2 with
+        | Some e', Some s1', Some s2' => Some $ Eif e' s1' s2'
+        | _, _, _ => None
+      end
+    | Ewhile c e s =>
+      match resolve_rhs ev c, resolve_rhs ev e, resolve_rhs ev s with
+        | Some c', Some e', Some s' => Some $ Ewhile c' e' s'
+        | _, _, _ => None
+      end
+    | Erete e => Erete <$> resolve_rhs ev e
+    | Eseq s1 s2 =>
+      match resolve_rhs ev s1, resolve_rhs ev s2 with
+        | Some s1', Some s2' => Some (Eseq s1' s2')
+        | _, _ => None
+      end
+  end.
+
+Fixpoint resolve_lhs (ev: env) (e: expr) : option expr :=
+  match e with
+    | Evalue (Vptr l) => Some e
+    | Evalue _ => None
+    | Ebinop _ _ _ => None (* XXX: too restrictive *)
+    | Ederef_typed _ _ => None
+    | Eaddrof _ => None
+    | Ealloc _ _ => None
+    | Evar x =>
+      (match sget x (lenv ev) with
+         | Some (_, l) => Some (Evalue (Vptr l))
+         | None => None
+       end)
+    | Ederef e' => resolve_rhs ev e'
+    | Efst e' => resolve_lhs ev e'
+    | Esnd e' =>
+      (e'' ← resolve_lhs ev e';
+       match type_infer ev e'' with
+         | Some (Tptr (Tprod t1 _)) =>
+           Some (Ebinop oplus e'' (Evalue (Vint8 (Byte.repr (sizeof t1)))))
+         | _ => None
+       end)
+    | Ecall f es => Some $ Ecall f es
+    | Eassign e1 e2 => e'' ← resolve_lhs ev e1; Some (Eassign e'' e2)
+    | Eif ex s1 s2 =>
+      match resolve_lhs ev s1, resolve_lhs ev s2 with
+        | Some s1', Some s2' => Some (Eif ex s1' s2')
+        | _, _ => None
+      end
+    | Ewhile c ex s => Ewhile c ex <$> resolve_lhs ev s
+    | Eseq s1 s2 =>
+      match resolve_lhs ev s1, resolve_lhs ev s2 with
+        | Some s1', Some s2' => Some (Eseq s1' s2')
+        | _, _ => None
+      end
+    | Erete e => Some (Erete e)
+  end.
+
+Fixpoint add_params_to_env (e: env) (params: list (ident * type)) ls : env :=
+  match params, ls with
+    | (x, t)::ps, l::ls' => add_params_to_env (Env (sset x (t, l) (lenv e)) (fenv e)) ps ls'
+    | _, _ => e
+  end.
+
+Definition instantiate_f_body (ev: env) (s: expr) : option expr :=
+  (resolve_rhs ev s ≫= resolve_lhs ev).
+
+Definition is_jump (e: expr) :=
+  match e with
+    | Ecall _ _ => true
+    | Erete _ => true
+    | _ => false
+  end.
+
 Inductive estep : expr → state → expr → state → Prop :=
 | ESbinop: ∀ lv rv op σ v',
              evalbop op lv rv = Some v' →
@@ -208,212 +325,51 @@ Inductive estep : expr → state → expr → state → Prop :=
       typeof v t →
       (∀ o', (s_heap σ) !! (b, o') = None) →
       estep (Ealloc t (Evalue v)) σ (Evalue (Vptr (b, o)))
-            (alloc_heap (b, o) v σ).
-
-(* Offset is ignored *)
-Inductive sstep : stmts → state → stmts → state → Prop :=
-| SSassign:
+            (alloc_heap (b, o) v σ)
+| ESassign:
     ∀ l v σ,
-      sstep (Sassign (Evalue (Vptr l)) (Evalue v))
-            σ
-            Sskip
+      estep (Eassign (Evalue (Vptr l)) (Evalue v))
+            σ (Evalue Vvoid)
             (State (storebytes l (encode_val v) (s_heap σ)) (s_text σ))
-| SSseq: ∀ s σ, sstep (Sseq Sskip s) σ s σ
-| SSseq_head:
-    ∀ s1 s1' s2 σ σ', sstep s1 σ s1' σ' → sstep (Sseq s1 s2) σ (Sseq s1' s2) σ'
+| ESbind:
+    ∀ e e' σ σ' k,
+      is_jump e = false →
+      estep e σ e' σ' →
+      estep (fill_expr e k) σ (fill_expr e' k) σ'
 .
+
+Inductive jstep: state → expr → list exprctx → expr → list exprctx → Prop :=
+| JSrete:
+    ∀ σ v k ks,
+      jstep σ (Erete (Evalue v)) (k::ks) (fill_expr (Evalue v) k) ks
+| JScall:
+    ∀ σ es ls retty params f_body f_body' f k ks,
+      es = map (fun l => Evalue (Vptr l)) ls →
+      s_text σ !! f = Some (Function retty params f_body) →
+      instantiate_f_body (add_params_to_env (Env [] []) params ls) f_body = Some f_body' →
+      jstep σ (fill_expr (Ecall f es) k) ks f_body' (k::ks).
 
 Bind Scope val_scope with val.
 Delimit Scope val_scope with V.
 Bind Scope expr_scope with expr.
 Delimit Scope expr_scope with E.
-Bind Scope stmts_scope with stmts.
-Delimit Scope stmts_scope with S.
 Bind Scope prim_scope with prim.
 Delimit Scope prim_scope with prim.
 
-Definition to_val (c: cureval) :=
-  match c with
-    | cure (Evalue v) => Some v
-    | curs Sskip => Some Vvoid
-    | _ => None
-  end.
+Inductive cstep: expr → state → list exprctx → expr → state → list exprctx → Prop :=
+| HSestep: ∀ e e' σ σ' ks, estep e σ e' σ' → cstep e σ ks e' σ' ks
+| HSjstep: ∀ e e' σ ks ks', jstep σ e ks e' ks' → cstep e σ ks e' σ ks'.
 
-Lemma of_to_val e v : to_val (cure e) = Some v → e = Evalue v.
-Admitted.
-
-Fixpoint s_to_ret_val (s: stmts) :=
-  match s with
-    | Sret => Some Vvoid
-    | Srete (Evalue v) => Some v
-    | Sseq s1 s2 => s_to_ret_val s1
-    | _ => None
-  end.
-
-Definition to_ret_val (c: cureval) :=
-  match c with
-    | curs s => s_to_ret_val s
-    | cure _ => None
-  end.
-
-Lemma fill_not_val Kes Ks e: to_val (curs (fill_stmts (fill_ectxs e Kes) Ks)) = None.
-Admitted.
-
-Lemma fill_not_ret_val Kes Ks e: to_ret_val (curs (fill_stmts (fill_ectxs e Kes) Ks)) = None.
-Admitted.
-
-Lemma val_implies_not_ret_val c v:
-  to_val c = Some v → to_ret_val c = None.
-Proof. intros ?. destruct c as [[]|[]]=>//. Qed.
-
-Lemma ret_val_implies_not_val c v:
-  to_ret_val c = Some v → to_val c = None.
-Proof. intros ?. destruct c as [[]|[]]=>//. Qed.
-
-(* Definition to_val (e: expr) := *)
-(*   match e with *)
-(*     | Evalue v => Some v *)
-(*     | _ => None *)
-(*   end. *)
-
-(* Definition of_val (v: val) := Evalue v. *)
-
-Fixpoint params_match (params: decls) (vs: list val) :=
-  match params, vs with
-    | (_, t)::params, v::vs => typeof v t ∧ params_match params vs
-    | [], [] => True
-    | _, _ => False
-  end.
-
-
-Fixpoint lift_list_option {A} (l: list (option A)): option (list A) :=
-  match l with
-    | Some x :: l' => (x ::) <$> lift_list_option l'
-    | None :: _ => None
-    | _ => Some []
-  end.
-
-Fixpoint resolve_rhs_e {t: Type} (ev: @env t) (e: expr) : option expr :=
-  match e with
-    | Evar x => (* closed-ness? *)
-      (match sget x (lenv ev) with
-         | Some (t, l) => Some $ Ederef_typed t (Evalue (Vptr l))
-         | None => None
-       end)
-    | Ebinop op e1 e2 =>
-      match resolve_rhs_e ev e1, resolve_rhs_e ev e2 with
-        | Some e1', Some e2' => Some (Ebinop op e1' e2')
-        | _, _ => None
-      end
-    | Ederef e =>
-      match type_infer ev e, resolve_rhs_e ev e with
-        | Some (Tptr t), Some e' => Some (Ederef_typed t e') 
-        | _, _ => None
-      end
-    | Eaddrof e => Eaddrof <$> resolve_rhs_e ev e
-    | Efst e => Efst <$> resolve_rhs_e ev e
-    | Esnd e => Esnd <$> resolve_rhs_e ev e
-    | Ecall f args => Ecall f <$> lift_list_option (map (resolve_rhs_e ev) args)
-    | _ => Some e
-  end.
-
-Fixpoint resolve_rhs {t: Type} (ev: @env t) (s: stmts) : option stmts :=
-  match s with
-    | Sskip => Some Sskip
-    | Sprim p => Some $ Sprim p
-    | Sassign e1 e2 => Sassign e1 <$> resolve_rhs_e ev e2
-    | Sif e s1 s2 =>
-      match resolve_rhs_e ev e, resolve_rhs ev s1, resolve_rhs ev s2 with
-        | Some e', Some s1', Some s2' => Some $ Sif e' s1' s2'
-        | _, _, _ => None
-      end
-    | Swhile c e s =>
-      match resolve_rhs_e ev c, resolve_rhs_e ev e, resolve_rhs ev s with
-        | Some c', Some e', Some s' => Some $ Swhile c' e' s'
-        | _, _, _ => None
-      end
-    | Srete e => Srete <$> resolve_rhs_e ev e
-    | Sret => Some Sret
-    | Sseq s1 s2 =>
-      match resolve_rhs ev s1, resolve_rhs ev s2 with
-        | Some s1', Some s2' => Some (Sseq s1' s2')
-        | _, _ => None
-      end
-  end.
-
-Fixpoint resolve_lhs_e {t: Type} (ev: @env t) (e: expr) : option expr :=
-  match e with
-    | Evar x =>
-      (match sget x (lenv ev) with
-         | Some (_, l) => Some (Evalue (Vptr l))
-         | None => None
-       end)
-    | Ederef e' => resolve_rhs_e ev e'
-    | Efst e' => resolve_lhs_e ev e'
-    | Esnd e' =>
-      (e'' ← resolve_lhs_e ev e';
-       match type_infer ev e'' with
-         | Some (Tptr (Tprod t1 _)) =>
-           Some (Ebinop oplus e'' (Evalue (Vint8 (Byte.repr (sizeof t1)))))
-         | _ => None
-       end)
-    | Evalue (Vptr l) => Some e
-    | Ecall f es => Some $ Ecall f es
-    | _ => None
-  end.
-
-Fixpoint resolve_lhs {t: Type} (e: @env t) (s: stmts) : option stmts :=
-  match s with
-    | Sassign e1 e2 => e'' ← resolve_lhs_e e e1; Some (Sassign e'' e2)
-    | Sif ex s1 s2 =>
-      match resolve_lhs e s1, resolve_lhs e s2 with
-        | Some s1', Some s2' => Some (Sif ex s1' s2')
-        | _, _ => None
-      end
-    | Swhile c ex s => Swhile c ex <$> resolve_lhs e s
-    | Sseq s1 s2 =>
-      match resolve_lhs e s1, resolve_lhs e s2 with
-        | Some s1', Some s2' => Some (Sseq s1' s2')
-        | _, _ => None
-      end
-    | Sret => Some Sret
-    | Srete e => Some (Srete e)
-    | Sprim p => Some $ Sprim p
-    | Sskip => Some Sskip
-  end.
-
-Fixpoint add_params_to_env {t: Type} (e: @env t) (params: list (ident * type)) ls : env :=
-  match params, ls with
-    | (x, t)::ps, l::ls' => add_params_to_env (Env _ (sset x (t, l) (lenv e)) (fenv e)) ps ls'
-    | _, _ => e
-  end.
-
-Definition instantiate_f_body {t: Type} (ev: @env t) (s: stmts) : option stmts :=
-  (resolve_rhs ev s ≫= resolve_lhs ev).
-
-Inductive cstep: cureval → state → list context → cureval → state → list context → Prop :=
-| CSestep: ∀ e e' σ σ' ks, estep e σ e' σ' → cstep (cure e) σ ks (cure e') σ' ks
-| CSsstep: ∀ s s' σ σ' ks, sstep s σ s' σ' → cstep (curs s) σ ks (curs s') σ' ks
-| CSecall:
-    ∀ es ls retty params f_body f_body' f ks σ,
-      es = map (fun l => Evalue (Vptr l)) ls →
-      s_text σ !! f = Some (Function stmts retty params f_body) →
-      instantiate_f_body (add_params_to_env (Env stmts [] []) params ls) f_body = Some f_body' →
-      cstep (cure (Ecall f es)) σ ks (curs f_body') σ ks.
-(* XXX: Essentially speaking, this prohibits free usage of global variables -- but this should be
-   easy to remedy *)
-
-Lemma fill_step_inv σ σ' ks ks' e c' Kes Ks:
-  to_val (cure e) = None →
-  to_ret_val (cure e) = None →
-  cstep (curs (fill_stmts (fill_ectxs e Kes) Ks)) σ ks c' σ' ks' →
-  (∃ e', c' = curs (fill_stmts (fill_ectxs e' Kes) Ks) ∧ estep e σ e' σ' ∧ ks = ks').
-Admitted.
+Lemma fill_estep_inv σ σ' ks ks' e c' Kes:
+  to_val e = None →
+  to_ret_val e = None →
+  cstep (fill_ectxs e Kes) σ ks c' σ' ks' →
+  (∃ e', c' =  (fill_ectxs e' Kes) ∧ estep e σ e' σ' ∧ ks = ks').
 
 Definition reducible cur σ ks := ∃ cur' σ' ks', cstep cur σ ks cur' σ' ks'.
 
-Lemma lift_reducible e Kes Ks σ ks:
-  reducible (cure e) σ ks → reducible (curs (fill_stmts (fill_ectxs e Kes) Ks)) σ ks.
+Lemma lift_reducible e Kes σ ks:
+  reducible e σ ks → reducible (fill_ectxs e Kes) σ ks.
 Admitted.
 
 Lemma reducible_not_val c σ ks: reducible c σ ks → to_val c = None.
@@ -440,11 +396,3 @@ Proof.
   { subst. intros. inversion H0; subst. constructor. }
   { intros. inversion H2; subst; constructor. }
 Qed.
-
-Lemma fill_seq_inv s1 s2 ks1 ks2 σ1 σ2 e2:
-  to_val (curs s1) = None →
-  to_ret_val (curs s1) = None →
-  cstep (curs (Sseq s1 s2)) σ1 ks1 e2 σ2 ks2 →
-  (∃ s1',
-     cstep (curs s1) σ1 ks1 (curs s1') σ2 ks2 ∧ e2 = curs (Sseq s1' s2) ∧ ks1 = ks2).
-Admitted.
