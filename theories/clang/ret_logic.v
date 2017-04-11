cc7bc087cec3ceb0eee1379d970f9f95aa4e39eb
Zhen Zhang <izgzhen@gmail.com>
2017-04-11 10:23:25 +0800

    Refine ret_logic

--- theories/clang/ret_logic.v
+++ theories/clang/ret_logic.v
@@ -12,10 +12,12 @@ Section wp_ret.
   (∃ eh K,
      ⌜to_val e1 = None ∧ unfill_expr e1 [] = Some (K, eh) ⌝ ∧
      ((⌜ is_jmp eh = false ⌝ ∗ WP eh @ E {{ v, wpr E (fill_ectxs (Evalue v) K) Φ Φret }}) ∨
-      (∃ v, ⌜ eh = Erete (Evalue v) ⌝ ∗ (stack_interp [[]] -∗ WP (Evalue v) @ E {{ Φret }})) ∨
-      (∃ f ls, ⌜ eh = Ecall f (map (λ l : addr, Evalue (Vptr l)) ls) ⌝ ∗
-               (stack_interp [[]] -∗
-                WP eh @ E {{ v, ▷ wpr E (fill_ectxs (Evalue v) K) Φ Φret }})))
+      (∃ v, ⌜ eh = Erete (Evalue v) ⌝ ∗ ▷ Φret v) ∨
+      (∃ f ls f_body f_body' params retty,
+         ⌜ eh = Ecall f (map (λ l : addr, Evalue (Vptr l)) ls) ∧
+           instantiate_f_body (add_params_to_env (Env [] []) params ls) f_body = Some f_body' ⌝ ∗
+         text_interp f (Function retty params f_body) ∗
+         (▷ wpr E f_body' (λ _, False)%I (λ v, wpr E (fill_ectxs (Evalue v) K) Φ Φret))))
   ))%I.
 
   Local Instance wpr_pre_contractive : Contractive wpr_pre.
@@ -32,19 +34,45 @@ Section wp_ret.
 
   Lemma wpr_helper E P R Q e eh K:
     unfill_expr e [] = Some (K, eh) →
-    (∀ v, P v ∗ R ⊢ Q v) →
+    (∀ v, P v -∗ R -∗ Q v) ∗
     ▷ R ∗
     WP eh @ E {{ P }}
     ⊢ WP eh @ E {{ Q }}.
   Proof.
-    iIntros (??) "[HR ?]".
-    assert (to_val eh = None) as ?; first admit.
-    iDestruct (wp_frame_step_l' with "[-]") as "?"=>//.
+    iIntros (?) "[? [HR ?]]".
+    assert (to_val eh = None) as ?.
+    { move : (cont_uninj' _ _ _ H0) => [? ?].
+      by apply enf_not_val. }
+    iDestruct (wp_frame_step_l' with "[-~]") as "?"=>//.
     { iFrame "HR". iFrame. }
     iApply (wp_strong_mono E E)=>//.
     iFrame. iIntros (?) "[? ?]".
-    iModIntro. iApply H1. iFrame.
-  Admitted.
+    iModIntro. iApply ("~" with "[-~1]")=>//.
+  Qed.
+
+  Lemma wpr_step_mono E eh (Φ1 Φ2: val → iProp Σ):
+    (□ (∀ v, Φ1 v -∗ Φ2 v) ∗ (wpr E eh (λ _ : val, False) Φ1)%I ⊢ wpr E eh (λ _ : val, False) Φ2).
+  Proof.
+    iLöb as "IH" forall (Φ1 Φ2 eh). rewrite !wpr_unfold /wpr_pre.
+    iIntros "[#? ?]". iDestruct "~1" as "[H | H]".
+    - by iDestruct "H" as (?) "[? %]".
+    - iDestruct "H" as (??) "[% [[% H] | [H | H]]]"; destruct H2.
+      + iRight. iExists _, _.
+        iSplit=>//. iLeft. iSplitL ""=>//.
+        iApply (wpr_helper)=>//. iFrame "IH". iFrame.
+        iIntros (?) "? ?".
+        iApply "~2". iFrame. done.
+      + iRight. iExists _, _.
+        iSplit=>//. iRight. iLeft. iDestruct "H" as (?) "[% ?]".
+        iExists _. iSplit=>//. iNext. by iApply "~".
+      + iRight. iExists _, _.
+        iSplit=>//. iRight. iRight. iDestruct "H" as (??????) "[% [? ?]]".
+        destruct H10. iExists _, _, _, _, _, _.
+        iSplit=>//. iFrame. iNext. iApply "IH". iFrame.
+        iAlways. iIntros (?) "?".
+        iApply "IH". iFrame.
+        done.
+  Qed.
 
   Lemma wpr_bind kes E e Φ Φret :
     wpr E e (fun v => wpr E (fill_ectxs (Evalue v) kes) Φ Φret) Φret
@@ -62,28 +90,27 @@ Section wp_ret.
           { by apply fill_ectxs_not_val. }
           { by apply unfill_app. }
         * iLeft. iSplitL ""; first done.
-          iApply wpr_helper=>//; last (iFrame "IH"; iFrame).
-          iIntros (?) "[? ?]".
-          iSpecialize ("~1" $! E (fill_ectxs (Evalue _) K) Φ Φret).
-          rewrite fill_app. by iApply "~1".
+          iApply wpr_helper=>//.
+          iFrame "IH". iFrame.
+          iIntros (?) "? ?".
+          rewrite -fill_app. iApply "~1". done.
       + iDestruct "~" as (v) "[% ?]". iRight. iExists eh, (kes ++ K). iSplit.
         * iPureIntro. split.
           { by apply fill_ectxs_not_val. }
           { by apply unfill_app. }
         * iRight. iLeft. iExists v. by iSplitL "".
-      + iDestruct "~" as (??) "[% ?]".
+      + iDestruct "~" as (??????) "[% [? ?]]". destruct H8.
         iRight. iExists eh, (kes ++ K). iSplit.
         * iPureIntro. split.
           { by apply fill_ectxs_not_val. }
           { by apply unfill_app. }
-        * iRight. iRight. iExists H2, H3.
-          iSplitL ""; first done. iIntros "?".
-          iDestruct ("~1" with "~") as "?".
-          iApply (wp_strong_mono E E)=>//.
-          iFrame. iIntros (?) "?".
-          iModIntro. iNext.
-          iSpecialize ("IH" $! E (fill_ectxs (Evalue a) K)).
-          rewrite fill_app. by iApply "IH".
+        * iRight. iRight. iExists _, _, _, _, _, _.
+          iSplitL ""; first done.
+          iFrame. iApply wpr_step_mono.
+          iFrame. iNext. iAlways.
+          iIntros (?) "?".
+          rewrite -fill_app. iApply "IH".
+          done.
   Qed.
 
   Lemma wpr_ret v E Φ Φret:
@@ -94,36 +121,8 @@ Section wp_ret.
     iRight. iExists (Erete (Evalue v)), [].
     iSplit.
     - iPureIntro. split=>//.
-    - iRight. iLeft. iExists v. iSplitL ""; first done.
-      iIntros "?".
-      by iApply wp_value.
+    - iRight. iLeft. iExists v. iSplitL ""=>//. eauto.
   Qed.
-
-  Lemma wp_something E e Φ:
-    wpr E e (λ _ : val, False) Φ
-    ⊢ WP e @ E {{ v, Φ v }}.
-  Proof.
-    iIntros "H". iLöb as "IH" forall (E e Φ). rewrite !wpr_unfold /wp_pre.
-    iDestruct "H" as "[H | H]".
-    - by iDestruct "H" as (v) "[_ %]".
-    - iDestruct "H" as (eh K) "(% & [[% ?]| [? | ?]])"; destruct H0.
-      + assert (e = fill_ectxs eh K) as ?; first admit.
-        subst. iApply wp_bind=>//.
-        iApply wpr_helper=>//; last (iFrame "IH"; iFrame).
-        iIntros (v) "[? IH]".
-        by iApply "IH".
-      + iDestruct "~" as (v) "[% ?]".
-        subst. assert (e = fill_ectxs (Erete (Evalue v)) K) as ?; first admit.
-        subst. iApply (wp_ret _ [] [[]]).
-        iSplitL ""; first admit.
-        iIntros "?".
-        simpl. by iDestruct ("~1" with "~") as "?".
-      + iDestruct "~" as (f ls) "[% ?]".
-        assert (e = fill_ectxs eh K) as ?; first admit. subst.
-        iAssert (stack_interp [[]]) as "H"; first admit.
-        iDestruct ("~1" with "H") as "?".
-        admit.
-  Admitted.
 
   Lemma wpr_call E es ls params f_body f_body' f retty Φ Φret:
     es = map (fun l => Evalue (Vptr l)) ls →
@@ -138,11 +137,13 @@ Section wp_ret.
     iSplit.
     { iPureIntro. split=>//. simpl.
       subst. by rewrite forall_is_val. }
-    iRight. iRight. iExists f, ls. subst. iSplitL ""; first done.
-    iIntros "?".
-    iApply (wp_call _ [])=>//.
-    iFrame. iNext. iIntros "?".
-    simpl. iApply wp_something.
-  Admitted.
+    iRight. iRight. iExists _, _, _, _, _, _. iFrame.
+    iSplitL "".
+    { iPureIntro. split=>//. by subst. }
+    iNext. iApply wpr_step_mono. iFrame.
+    iAlways. iIntros (?) "?".
+    simpl. rewrite wpr_unfold /wpr_pre.
+    iLeft. eauto.
+  Qed.
 
 End wp_ret.
