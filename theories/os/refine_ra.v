62aca138405393361f2050849b690a0430fc1b61
Zhen Zhang <izgzhen@gmail.com>
2017-04-13 10:30:08 +0800

    Finish another homework

--- theories/os/refine_ra.v
+++ theories/os/refine_ra.v
@@ -53,7 +53,7 @@ Section algebra.
 
   Inductive refine_disjoint : Disjoint refine_car :=
   | snap_snap_disjoint cs1 cs2 :
-      (prefix cs1 cs2 ∨ prefix cs2 cs1) →
+      (suffix cs1 cs2 ∨ suffix cs2 cs1) →
       Refine snapshot cs1 ⊥ Refine snapshot cs2
   | snap_master_disjoint cs1 cs2:
       Refine snapshot cs1 ⊥ Refine master (cs2 ++ cs1)
@@ -74,7 +74,7 @@ Section algebra.
   Instance op_refine : Op refine_car :=
     λ r1 r2, Refine (refine_view r1 ⋅ refine_view r2) (cfgs r1 ⋅ cfgs r2).
 
-  Lemma refine_op v1 v2 l1 l2: Refine v1 l1 ⋅ Refine v2 l2 = Refine (v1 ⋅ v2) (l1 ⋅ l2).
+  Lemma refine_op v1 v2 l1 l2: Refine v1 l1 ⋅ Refine v2 l2 = Refine (max_view v1 v2) (l1 ⋅ l2).
   Proof. unfold op, op_refine. by simpl. Qed.
 
   Lemma cfgs_id_merge (cs: list cfg): cs ⋅ cs = cs.
@@ -91,11 +91,23 @@ Section algebra.
     [ constructor | apply app_nil_l ].
   Qed.
 
-  Lemma op_cfgs_prefix (l1 l2: list cfg): prefix l1 l2 → l1 ⋅ l2 = l2.
+  Lemma op_cfgs_suffix (l1 l2: list cfg): suffix l1 l2 → l1 ⋅ l2 = l2.
   Admitted.
 
   Lemma op_cfgs_app (l1 l2: list cfg): (l1 ++ l2) ⋅ l2 = l1 ++ l2.
   Admitted.
+
+  Lemma suffix_app (l1 l2 lx ly: list cfg):
+    l1 ++ lx = l2 ++ ly →                
+    lx `suffix_of` ly ∨ ly `suffix_of` lx.
+  Admitted.
+
+  Lemma suffix_trans (l1 l2 l3: list cfg):
+    suffix l1 l2 → suffix l2 l3 → suffix l1 l3.
+  Proof.
+    intros [? ?] [? ?].
+    simplify_eq. rewrite assoc. by eexists _.
+  Qed.
 
   Lemma refine_dra : DRAMixin refine_car.
   Proof.
@@ -106,11 +118,34 @@ Section algebra.
       + by rewrite op_cfgs_app.
       + by rewrite comm_op_cfgs op_cfgs_app.
       + destruct H4.
-        * by rewrite op_cfgs_prefix.
-        * by rewrite comm_op_cfgs op_cfgs_prefix.
-    - intros ???.
-
-    - admit.
+        * by rewrite op_cfgs_suffix.
+        * by rewrite comm_op_cfgs op_cfgs_suffix.
+    - intros x y z. destruct x, y, z.
+      by rewrite !refine_op !assoc.
+    - intros [[] lx] [[] ly] [[] lz]; rewrite !refine_op; simpl; intros=>//;
+      match goal with
+        | [H : Refine master _ ⊥ Refine master _ |- _ ] => inversion H
+        | _ => idtac
+      end; inversion H2; inversion H3; simplify_eq.
+      + rewrite op_cfgs_app in H7.
+        rewrite -H7. constructor.
+      + rewrite comm_op_cfgs op_cfgs_app in H7.
+        constructor. eapply suffix_app. by symmetry.
+      + destruct H6.
+        * rewrite op_cfgs_suffix in H3=>//.
+          rewrite op_cfgs_suffix=>//. inversion H4.
+          subst. rewrite assoc. constructor.
+        * rewrite comm_op_cfgs op_cfgs_suffix in H3=>//.
+          rewrite comm_op_cfgs op_cfgs_suffix=>//.
+      + constructor.
+        destruct H6.
+        * rewrite op_cfgs_suffix in H9=>//.
+          inversion H9.
+          { left. eapply suffix_trans=>//. }
+          inversion H4. inversion H5.
+          rewrite H6 in H7.
+          by eapply suffix_app.
+        * rewrite comm_op_cfgs op_cfgs_suffix in H9=>//.
     - admit.
     - admit.
     - admit.
